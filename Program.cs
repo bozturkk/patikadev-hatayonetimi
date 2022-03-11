e84da9d300ffa9a099e7bb30e8df110c6fb3a7bd
bozturkk <89193311+bozturkk@users.noreply.github.com>
2022-03-11 17:11:12 +0300

    Ders tamamlandı.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HataYonetimi // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+               Console.WriteLine("Bir sayı giriniz");
+               int sayi = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Girdiğiniz sayı : {0}", sayi);
+           }
+           catch (Exception ex)
+           {
+               
+               Console.WriteLine("Hata :" +ex.Message.ToString());
+           }
+           finally
+           {
+               Console.WriteLine("İşlem tamamlandı.");
+           }
+
+           try
+           {
+               //parse string değerleri int e çevirmek için kullanılır.
+               //int a = int.Parse(null);
+               int b = int.Parse("-20000000000");
+           }
+           catch (ArgumentNullException ex)
+           {
+               Console.WriteLine("Boş değer girdiniz");
+               Console.WriteLine(ex);
+           }
+           catch (OverflowException ex)
+           {
+               Console.WriteLine("Kapsam dışı bir değer girildi.");
+               Console.WriteLine(ex);
+           }
+           finally
+           {
+               Console.WriteLine("İşlem başarı ile tamamlandı.");
+           }
+
+        }
+    }
+}
